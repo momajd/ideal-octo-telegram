1e50471818927897427c06b27e4e4664488628ec
momajd <mo.a.majd@gmail.com>
2017-11-24 14:53:08 -0500

    refactor displacement solver

--- app/models/truss.rb
+++ app/models/truss.rb
@@ -24,56 +24,52 @@ class Truss < ApplicationRecord
 
     self.members.each do |member|
       length, lambda_x, lambda_y = member.length, member.lambda_x, member.lambda_y
-      AE = member.area * member.elastic_modulus
+      area, elastic_mod = member.material.area, member.material.elastic_modulus
 
       i1 = member.near_node.x_degree_of_freedom.matrix_row
       i2 = member.near_node.y_degree_of_freedom.matrix_row
       i3 = member.far_node.x_degree_of_freedom.matrix_row
       i4 = member.far_node.y_degree_of_freedom.matrix_row
 
-      matrix[i1, i1] += AE * lambda_x**2 / length
-      matrix[i1, i2] += AE * lambda_x * lambda_y / length
-      matrix[i1, i3] += AE * -lambda_x**2 / length
-      matrix[i1, i4] += AE * -lambda_x * lambda_y / length
+      matrix[i1, i1] += area * elastic_mod * lambda_x**2 / length
+      matrix[i1, i2] += area * elastic_mod * lambda_x * lambda_y / length
+      matrix[i1, i3] += area * elastic_mod * -lambda_x**2 / length
+      matrix[i1, i4] += area * elastic_mod * -lambda_x * lambda_y / length
 
-      matrix[i2, i1] += AE * lambda_x * lambda_y / length
-      matrix[i2, i2] += AE * lambda_y**2 / length
-      matrix[i2, i3] += AE * -lambda_x * lambda_y / length
-      matrix[i2, i4] += AE * -lambda_y**2 / length
+      matrix[i2, i1] += area * elastic_mod * lambda_x * lambda_y / length
+      matrix[i2, i2] += area * elastic_mod * lambda_y**2 / length
+      matrix[i2, i3] += area * elastic_mod * -lambda_x * lambda_y / length
+      matrix[i2, i4] += area * elastic_mod * -lambda_y**2 / length
 
-      matrix[i3, i1] += AE * -lambda_x**2 / length
-      matrix[i3, i2] += AE * -lambda_x * lambda_y / length
-      matrix[i3, i3] += AE * lambda_x**2 / length
-      matrix[i3, i4] += AE * lambda_x * lambda_y / length
+      matrix[i3, i1] += area * elastic_mod * -lambda_x**2 / length
+      matrix[i3, i2] += area * elastic_mod * -lambda_x * lambda_y / length
+      matrix[i3, i3] += area * elastic_mod * lambda_x**2 / length
+      matrix[i3, i4] += area * elastic_mod * lambda_x * lambda_y / length
 
-      matrix[i4, i1] += AE * -lambda_x * lambda_y / length
-      matrix[i4, i2] += AE * -lambda_y**2 / length
-      matrix[i4, i3] += AE * lambda_x * lambda_y / length
-      matrix[i4, i4] += AE * lambda_y**2 / length
+      matrix[i4, i1] += area * elastic_mod * -lambda_x * lambda_y / length
+      matrix[i4, i2] += area * elastic_mod * -lambda_y**2 / length
+      matrix[i4, i3] += area * elastic_mod * lambda_x * lambda_y / length
+      matrix[i4, i4] += area * elastic_mod * lambda_y**2 / length
     end
     matrix
   end
 
   def solve_displacements!
-    free_dofs = degree_of_freedoms.select {|dof| !dof.fixed}
-    subset_i = free_dofs.max_by {|dof| dof.matrix_row}.matrix_row
+    free_dofs = degree_of_freedoms.select {|dof| !dof.fixed}.sort_by {|dof| dof.matrix_row}
+    subset_i = free_dofs[-1].matrix_row
 
     sub_stiffness_matrix = stiffness_matrix.minor(0..subset_i, 0..subset_i)
 
     applied_loads = []
-    dofs = []
     (0..subset_i).each do |i|
-      dof = x_degree_of_freedoms.find_by(matrix_row: i)
-      dof = y_degree_of_freedoms.find_by(matrix_row: i) unless dof
-
+      dof = free_dofs[i]
       applied_loads << dof.node.total_load(dof.direction)
-      dofs << dof
     end
 
     augmented_matrix = sub_stiffness_matrix.augment(applied_loads)
     displacements  = augmented_matrix.gauss_jordan
 
-    displacements.each_with_index {|delta, i| dofs[i].update!(displacement: delta)}
+    displacements.each_with_index {|delta, i| free_dofs[i].update!(displacement: delta)}
   end
 
   def solve_reactions!
